0b2691db044da0de8bb68410565cfa9c14b1450b
Trung Mai <emailmehurry@yahoo.com>
2021-11-19 22:17:42 -0800

    Cleaned up and improved codes

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                // Event being fired when a delivery of message to the consumer channel on "BasicTest" queue and data/payload of the event is in ea (event arguments object)
+               
+                consumer.Received += (model, ea) =>
+                {
+
+                    // Converting to array of bytes
+                    byte[] body = ea.Body.ToArray();
+                    // Encoding the message into UTF8 format string
+                    var message = Encoding.UTF8.GetString(body);
+                    if (message.Contains("number 1."))
+                    {
+                        Console.Clear();
+                    }
+                    var messageParts = message.Split(' ');
+                    // Wrting out the received message
+                    Console.WriteLine("Received message {0} {1}: {2}", messageParts[messageParts.Length - 2], messageParts[messageParts.Length - 1].Replace(".", null),  message);
+                   
+                };
+
+                // Consuming the message on the channel of queue "BasicTest", auto acknowledge, and passing in the consumer
+                channel.BasicConsume("BasicTest", true, consumer);
+                Console.WriteLine("Press [Enter] to exit the Consumer.");
+                Console.ReadLine();
+               
+            }
+            
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client; // Need for RabbitMQ messaging
+using System;
+using System.Text; // Need for text message
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Creating a connection factory
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            // Creating a connection
+            using (var connection = factory.CreateConnection())
+            // Creating a channel and sending a message to the queue on RabbitMQ
+            using (var channel = connection.CreateModel())
+            {
+                var input = string.Empty;
+                bool resultOfParse;
+                int parsedNumber;
+                string message = string.Empty;
+                byte[] body = null;
+
+
+                while (input.Trim().ToUpper() != "X".ToString())
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter number of messages to send or press letter X to exit the Sender");
+                    input = Console.ReadLine();
+                    resultOfParse = int.TryParse(input.ToString(), out parsedNumber);
+                    while (!resultOfParse && input.Trim().ToUpper() != "X".ToString())
+                    {
+                        Console.WriteLine("Enter VALID number of messages to send or press letter X to exit the Sender");
+                        input = Console.ReadLine();
+                        resultOfParse = int.TryParse(input.ToString(), out parsedNumber);
+                    }
+
+                    if (input.Trim().ToUpper() != "X".ToString())
+                    {
+                        for (int i = 1; i <= parsedNumber; ++i)
+                        {
+                            // Creating a message queue named BasicTest
+                            channel.QueueDeclare("BasicTest", false, false, false, null);
+                            message = $"My message to RabbitMQ using .Net Core RabbitMQ message number {i}.";
+                            // Encoding the message into bytes array
+                            body = Encoding.UTF8.GetBytes(message);
+                            // Sending message containing in the variable body to the queue name "BasicTest" as routingKey using default exchange name indicated by ""
+                            // 
+                            channel.BasicPublish("", "BasicTest", null, body);
+                            Console.WriteLine("Sent message number {0}: {1}", i, message);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Press X to exit sender. Otherwise, press any other key to continue sending message(s).");
+                        var c = Console.ReadKey();
+                        input = c.Key.ToString();
+                    }
+                    
+                    
+                }
+            }
+            
+            
+        }
+    }
+}
